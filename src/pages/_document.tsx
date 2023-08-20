bfd722c4b2d50fa55ad13afd99ab3725b9bb3970
thilina4321 <dilshanthilina53@gmail.com>
2023-08-21 01:05:20 +0530

    PWA with next Js

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+      <link rel="apple-touch-icon" href="/icon.png" />
+      <link rel="manifest" href="/manifest.json" />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
